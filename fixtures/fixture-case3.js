72fd2b8a749a4023d838ca43618b402f9f0f326c
이영창(piknow) <apricotsoul@gmail.com>
2025-04-18 15:24:38 +0900

    feat: js-codeshif 3 케이스 추가

---
+++ fixtures/fixture-case3.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text } from "react-native";
+
+// Case 3: Only Text is imported from react-native
+const Component3 = () => {
+  return (
+    <>
+      <Text>Hello from Component3!</Text>
+    </>
+  );
+};
+
+export default Component3;
